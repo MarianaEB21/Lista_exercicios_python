f8610e1e77194975aa3a7f4bd123b7e2f03f37d6
maria <mariana_eiroz@hotmail.com>
2022-09-24 14:13:19 -0300

    Melhorando lógica do exercício 01

--- RM96482_EX01.py
+++ RM96482_EX01.py
@@ -8,49 +8,52 @@
 #   que calcule e exiba qual o valor do bônus que o cliente deve pagar a vocês. A tabela abaixo mostra a porcentagem
 #   de acordo com cada nível de assinatura:
 
-###########################
-# Passos:
-# - definir variáveis (plano & faturamento)
-# **** (O que podemos fazer antes de verificar o plano???)
-# - verificar porcentagem
-# - calcular bônus
-# - imprimir resultado
-
+# Definindo variáveis
 plano = 0
 faturamento_anual: float
 porcentagem: float
 bonus: float
 
-while plano < 1 or plano > 4:
+while (plano < 1 or plano > 4):
     print("PLANOS DISPONÍVEIS")
     print("1 - Basic")
     print("2 - Silver")
     print("3 - Gold")
     print("4 - Platinum")
-    plano = int(input("Digite o número correspondente ao seu plano: "))
 
-    if plano == 1:
-        print("Plano Basic")
-        porcentagem = 0.3
+    try:
+        plano = int(input("Digite o número correspondente ao seu plano: "))
+    except:
+        print('!!! Digite um valor numérico !!!')
 
-    elif plano == 2:
-        print("Plano Silver")
-        porcentagem = 0.2
+if plano == 1:
+    print(">> Plano Basic")
+    porcentagem = 0.3
 
-    elif plano == 3:
-        print("Plano Gold")
-        porcentagem = 0.1
+elif plano == 2:
+    print(">> Plano Silver")
+    porcentagem = 0.2
 
-    elif plano == 4:
-        print("Plano Platinum")
-        porcentagem = 0.05
+elif plano == 3:
+    print(">> Plano Gold")
+    porcentagem = 0.1
 
-    else:
-        print("Opção inválida!")
+elif plano == 4:
+    print(">> Plano Platinum")
+    porcentagem = 0.05
+else:
+    print("Opção inválida!")
 
-    faturamento_anual = float(input("Qual seu faturamento anual? "))
-    bonus = faturamento_anual * porcentagem
-    print(f"Seu bônus será R$ {bonus}")
+error_faturamento = True
+while error_faturamento:
+    try:
+        faturamento_anual = float(input("Qual seu faturamento anual? "))
+        bonus = faturamento_anual * porcentagem
+        print(f"Seu bônus será R$ {bonus}")
+        error_faturamento = False
+    except:
+        print("O faturamento precisa ser numérico!")
+        error_faturamento = True
 
 
 
